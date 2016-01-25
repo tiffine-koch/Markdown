02c97695b948e29f8e9586568a0297229929a450
Apple <tiffine.koch@gmail.com>
2016-01-25 08:45:56 -0800

    checked indentation and syntax

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var PORT = 4000;
 
-// bring in dependencies / libraries
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
@@ -10,28 +9,18 @@ var fs = require('fs');
 var app = express();
 var marked = require('marked');
 
-// configure general middleware
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// route definitions
 app.get('/', function(req, res) {
   var html = fs.readFileSync('./index.html').toString();
   res.send(html);
 });
 
-// app.post('/markdown', function(req, res) {
-//   var markdown = req.body.markdown;
-//
-//   var markdownText = marked(markdown);
-//   res.send(markdownText);
-// });
-
 app.use('/markdown', require('./router/mark.js'));
 
-// spin up server
 app.listen(PORT, function() {
   console.log('Express server listening on port', PORT)
 });
